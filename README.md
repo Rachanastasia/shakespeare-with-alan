6014040c847449d9bccf1b773977e8f8abe1e107
Rachel Reilly <r.a.reill18@gmail.com>
2021-01-21 16:13:57 -0600

    removed portal and put instance in App.js

--- README.md
+++ README.md
@@ -1,13 +1,7 @@
-# NEW React Boilerplate
-
-This boilerplate:
-
-- Removes unnecessary files from `create-react-app`
-- Sets up css, components, contexts folders
-- Sets up variables.css and main.css and imports into App.js
-- Creates header and renders at top of screen
+# Alan Playground
 
 ## Tech
 
 - React.js
 - CSS3
+- Alan Voice Assistant
--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import './css/variables.css';
 import './css/main.css';
 import './css/other.css';
 import Header from './components/Header';
-import AlanButton from './components/AlanButton';
-
+//import AlanButton from './components/AlanButton';
+import alanBtn from "@alan-ai/alan-sdk-web";
+import intent from '@alan-ai/alan-sdk-web';
 
 function App() {
+  const alanBtnInstance = useRef(null);
+
+  intent("go back", p => {
+    p.play("Testing this is cool");
+    p.play({ command: "go:back" });
+  })
+
+  useEffect(() => {
+    if (!alanBtnInstance.current) {
+      alanBtnInstance.current = alanBtn({
+        key: "78907422e81aa61487f9b5348554e7192e956eca572e1d8b807a3e2338fdd0dc/stage",
+        rootEl: document.getElementById('alan-button'),
+        onCommand: (commandData) => {
+          if (commandData.command === "go:back") {
+            console.log('WENT BACK')
+          }
+        }
+
+      })
+    }
+
+  }, [])
   return (
     <main className="App">
       <Header />
-      <AlanButton />
     </main>
   );
 }
